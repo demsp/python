de81420bb2a849d0a4f0d4071b78d3baed230d05
demsp <dserp18@gmail.com>
2018-07-04 19:52:10 +0300

    Create two_sages.py

---
+++ two_sages.py
@@ -0,0 +1,13 @@
+>>>pr=[2,3,5,7]
+>>> z=[]
+>>> for x in pr:
+	    for y in pr:
+		    z.append(x*y)
+>>> z=sorted(z)
+>>> z1=[]
+>>> while i<len(z):
+	    if z[i]>10:
+		    z1.append(z[i])
+		    del z[i]
+	    else:
+		    i+=1
