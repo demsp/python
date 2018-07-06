7c61cfb267a05e7797a5093dfeb9877b2a7a6322
Dima <dserp18@gmail.com>
2018-07-06 13:49:16 +0300

    Update sultans_logic_task.py

--- sultans_logic_task.py
+++ sultans_logic_task.py
@@ -1,4 +1,4 @@
->>> prime_nums=[3,5,7,11,13,17,19]
+>>> prime_nums=[2,3,5,7,11,13,17,19]
 >>> def mult_nums(nums):
 	    arr=[]
 	    for i in nums:
@@ -21,7 +21,22 @@
 	    return nums  
 >>> sort_nums(del_same(mult_nums(prime_nums)))
 [9, 15, 21, 25, 33, 35, 39, 49, 51, 55, 57, 65, 77, 85, 91, 95, 119, 121, 133, 143, 169, 187, 209, 221, 247, 289, 323, 361]      
-      
+ >>> def sum_nums(nums):
+	arr=[]
+	for i in nums:
+		for j in nums:
+			arr.append(i+j)
+	return arr
+>>> def subtact_nums(nums1,nums2):
+	arr=[]
+	for i in nums1:
+		if not i in nums2:
+			arr.append(i)
+	return arr
+
+>>> subtact_nums(range(4,41),sort_nums(del_same(sum_nums(prime_nums))))
+[11, 17, 23, 25, 27, 29, 31, 33, 35, 37, 39, 40]
+
       
       
       
